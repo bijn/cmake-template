6585864afa8c5b63c2085c4c1585f591ad4b7943
Bijan Sondossi <bijan.n.sondossi@gmail.com>
2018-06-22 17:33:47 -0700

    generate project script

---
+++ generate_project.sh
@@ -0,0 +1,27 @@
+# Bijan Sondossi
+# generate_project.sh
+# Generates a basic project from the default template.
+
+if [ -z "$1" ] && ! [ -e "$1" ]
+then
+    echo "USAGE: ./generate_project.sh project_name"
+    exit -1
+fi
+
+if mkdir -p $1/modules && cp -r template/* $1 && cd $1
+then
+    git init
+    git submodule add --name gtest \
+        https://github.com/google/googletest modules/gtest
+    git submodule add --name cmake-helpers \
+        https://github.com/bijn/cmake-helpers modules/cmake-helpers
+
+    if [[ "$unamestr" == 'Linux' ]]
+    then
+        sed -i "s/Template/$1/g" CMakeLists.txt
+        sed -i "s/TEMPLATE/$1/g" include/Config.hh.in
+    else
+        sed -i '' "s/Template/$1/g" CMakeLists.txt
+        sed -i '' "s/TEMPLATE/$1/g" include/Config.hh.in
+    fi
+fi
