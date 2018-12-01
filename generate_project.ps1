480cee4a2c6bb9b38029998e75efb88b952962b7
Bijan Sondossi <bijan.n.sondossi@gmail.com>
2018-11-30 20:57:45 -0800

    Closes #3, updated PowerShell script

--- generate_project.ps1
+++ generate_project.ps1
@@ -1,5 +1,5 @@
 # Parker Woodard
-# generate_project.ps1 
+# generate_project.ps1
 # Generates a basic project from the default template. (Powershell)
 
 Param
@@ -9,20 +9,23 @@ Param
     $project_name
 )
 
-if(!(Test-Path -Path $project_name)) 
+if(!(Test-Path -Path $project_name))
 {
     mkdir $project_name/modules
     Copy-Item template/* -Destination $project_name -Recurse
     cd $project_name
+
+    Get-ChildItem -Recurse "." -include *.* | Foreach-Object {
+        $file = $_.FullName
+        (Get-Content $file).replace('Template', $project_name) | Set-Content $file
+        (Get-Content $file).replace('TEMPLATE', $project_name) | Set-Content $file
+    }
 
     git init
     git submodule add --name gtest `
         https://github.com/google/googletest modules/gtest
     git submodule add --name cmake-helpers `
         https://github.com/bijn/cmake-helpers modules/cmake-helpers
-
-    (Get-Content CMakeLists.txt).replace('Template', $project_name) | Set-Content CMakeLists.txt
-    (Get-Content include/Config.hh.in).replace('TEMPLATE', $project_name) | Set-Content include/Config.hh.in
 }
 else
 {
